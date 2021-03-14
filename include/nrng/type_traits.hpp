638f03e7ab13f65054c67d2b68b345cf87fe1687
Florian Weik <fweik@icp.uni-stuttgart.de>
2021-03-14 15:48:09 +0100

    unary_closedd_under concept

--- include/nrng/type_traits.hpp
+++ include/nrng/type_traits.hpp
@@ -18,6 +18,12 @@ template <class T, class BinaryOp>
 concept binary_closed_under = requires(BinaryOp op, T t) {
   { op(t, t) } -> std::convertible_to<T>;
 };
+
+/** @brief UnaryOp: T -> T. */
+template <class T, class UnaryOp>
+concept unary_closed_under = requires(UnaryOp op, T t) {
+  { op(t) } -> std::convertible_to<T>;
+};
 } // namespace nrng
 
 #endif
